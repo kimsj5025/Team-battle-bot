a439e82be12969b50be21e05134e83391b9c6269
kimsj5025 <kimsj5025@naver.com>
2020-12-09 09:40:08 +0900

    ./text, {} fun

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ var config = require('./Config.json'); //config.json 파일을 불러옴
 var fs = require('fs'); //fs 파일을 불러옴
 var game = 'Team battle bot'
 var i = 0
-fs.mkdir('./text')
-console.log(`Starting bot...`);
+fs.mkdir('./text',{}function () {  
+  console.log(`Starting bot...`);
+})
 
 
 client.on('ready', () => {
