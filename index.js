b55c9f45555cf94e8788af16643814431c135333
kimsj5025 <kimsj5025@naver.com>
2020-12-10 16:48:45 +0900

    ㅇㅇㅇㅇ

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var config = require('./Config.json'); //config.json 파일을 불러옴
 var fs = require('fs'); //fs 파일을 불러옴
 var game = 'Team battle bot'
 var i = 0
+var e = ''
 fs.mkdir('./info',{}, function () {
   console.log(`Starting bot...`);
 })
@@ -26,11 +27,10 @@ client.on('message', msg => {
 
 
   if (msg.author.username !== myname[0]) {
-
 /*------------------------------------------------------------------------------------*/
     if (msg.content.startsWith('!add')) {
     var code = msg.content.split(' ') //coed =array
-    var sentence = code[2]
+    var sentence = ''
     var i = 2
     while (i < code.length) {
 
@@ -38,8 +38,8 @@ client.on('message', msg => {
       i++
     }
     var sentence = sentence + '\n'
-    fs.writeFile(`info/${code[1]}`,sentence,function(err){
-       if (err === null) {
+    fs.writeFile(`info/${code[1]}`,sentence,function(e){
+       if (e === null) {
 
        console.log('writeFile is success!');
        msg.channel.send(`${msg.author.username},\nYour level is submitted!
@@ -59,21 +59,18 @@ Level style : ${code[2]}`)
   }
 /*------------------------------------------------------------------------------------*/
     if (msg.content === '!list') {
-      fs.readdir('./info', function(error, filelist){
+      fs.readdir('./info', function(e, filelist){
         if (filelist === '') {
           msg.channel.send(`\nWe have no level`)
         }else {
-          var filelist = filelist + '\n'
-          var filelist = filelist.split('\n')
           var i = 0
           var dirs = `We're level list is\n`
           while (i < filelist.length) {
             //filelist는 맵 코드
-            var data = fs.readFileSync(`./info/${filelist[i]}`);
-            var dirs = dirs + '\n' +filelist[i]
-            var data = data + '\n'
+            var data = fs.readFileSync(`./info/${filelist[i]}`, 'utf8');
             var data = data.split('\n')
-            var dirs = `${dirs} - \`${data[2]}\``
+            var dirs = dirs + '\n' +filelist[i]
+            var dirs = `\n${dirs} - \`${data[2]}\``
             i++
           }
           msg.channel.send(dirs)
@@ -93,31 +90,24 @@ Level style : ${code[2]}`)
     }
 /*------------------------------------------------------------------------------------*/
     if (msg.content.startsWith('!info')) {
-      try {
+      var code = msg.content.split(' ') //coed =array
+      fs.readFile(`./info/${code[1]}`, 'utf8', function(e, data) {
+        if (msg.content === '!info') {
 
-        var code = msg.content.split(' ') //coed =array
-        fs.readFile(`./info/${code[1]}`, 'utf8', function(err, data) {
+          msg.channel.send('No matching results')
 
-          if (msg.content === '!info') {
+        } else {
+          var data = data + '\n'
+          var data = data.split('\n')
+          var i = 0
+          var info = `${code[1]}'s info is...\n`
+          while (i < data.length) {
+              var info = `${info}\n${data[i]}`
+              i++
+          }
 
-            msg.channel.send('No matching results')
-
-          } else {
-            var data = data + '\n'
-            var data = data.split('\n')
-            var i = 0
-            var info = `${code[1]}'s info is...\n`
-            while (i < data.length) {
-                var info = `${info}\n${data[i]}`
-                i++
-              }
-
-            }
-        })
-
-      } catch (e) {
-        console.error(e);
-      }
+        }
+      })
     }
 /*------------------------------------------------------------------------------------*/
 //LISTENING, WATCHING, PLAYING
@@ -134,36 +124,55 @@ Level style : ${code[2]}`)
     console.log(`Game has updata by ${msg.author.username}`);
   }
     if(msg.content.startsWith('!listen ')) {
-  var code = msg.content.split(' ') //coed =array
-  msg.channel.send('Game is update!')
-  var game = ''
-  var i = 1
-  while (i < code.length) {
-    var game = game + ' ' +code[i]
-    i++
-  }
-  client.user.setActivity(game, { type: 'LISTENING' })
-  console.log(`Game has updata by ${msg.author.username}`);
-  }
+      var code = msg.content.split(' ') //coed =array
+      msg.channel.send('Game is update!')
+      var game = ''
+      var i = 1
+      while (i < code.length) {
+        var game = game + ' ' +code[i]
+        i++
+      }
+      client.user.setActivity(game, { type: 'LISTENING' })
+      console.log(`Game has updata by ${msg.author.username}`);
+    }
     if(msg.content.startsWith('!play ')) {
-  var code = msg.content.split(' ') //coed =array
-  msg.channel.send('Game is update!')
-  var game = ''
-  var i = 1
-  while (i < code.length) {
-    var game = game + ' ' +code[i]
-    i++
-  }
-  client.user.setActivity(game, { type: 'PLAYING' })
-  console.log(`Game has updata by ${msg.author.username}`);
-  }
+      var code = msg.content.split(' ') //coed =array
+      msg.channel.send('Game is update!')
+      var game = ''
+      var i = 1
+      while (i < code.length) {
+        var game = game + ' ' +code[i]
+        i++
+      }
+      client.user.setActivity(game, { type: 'PLAYING' })
+      console.log(`Game has updata by ${msg.author.username}`);
+    }
+/*------------------------------------------------------------------------------------*/
+    if(command === '!Delete') {
+      console.log('woking');/*
+      var data = command.split(' ')
+      console.log(data);
+      fs.unlink(`./info/${data[1]}`, function (e) {
+        if (e == null) {
+          msg.channel.send(`Delete level ${data[1]}`)
+        }else {
+          msg.channel.send(`error! \n${e}`)
+        }
+      });*/
+    }
 /*------------------------------------------------------------------------------------*/
     if(command === '!code') {
       msg.channel.send(`Team battle bot\'s source code is
 https://github.com/kimsj5025/Team-battle-bot.git`)
     }
 /*------------------------------------------------------------------------------------*/
-
+    if (command === '!debug') {
+      if (e !== '') {
+        msg.channel.send(`last error is \n\n${e}`)
+      }else {
+        msg.channel.send(`last error is not fund`)
+      }
+    }
 
 
 
@@ -178,4 +187,4 @@ https://github.com/kimsj5025/Team-battle-bot.git`)
 }); //client.on
 
 
-client.login('Nzc4NTg1MDQyMzg5NTY1NDYw.X7UH0w.azh-6GyPrqBz5fhiY2lqrGb0d2k');
+client.login('Nzc4NTg1MDQyMzg5NTY1NDYw.X7UH0w.olFGPvUTPsujCle82Aif3UGJNcU');
